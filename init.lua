5c98552d7c0c136498a8320f937e14ecafc0fff3
souji <timoqueezle@gmail.com>
2025-06-01 15:55:10 +0200

    chore: undotree, fugitive config

--- init.lua
+++ init.lua
@@ -925,6 +925,29 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[G]it [S]tatus' })
     end,
   },
+  {
+    'theprimeagen/harpoon',
+    name = 'harpoon',
+    config = function()
+      local mark = require 'harpoon.mark'
+      local ui = require 'harpoon.ui'
+
+      vim.keymap.set('n', '<leader>a', mark.add_file)
+      vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+      vim.keymap.set('n', '<C-h>', function()
+        ui.nav_file(1)
+      end)
+      vim.keymap.set('n', '<C-j>', function()
+        ui.nav_file(2)
+      end)
+      vim.keymap.set('n', '<C-k>', function()
+        ui.nav_file(3)
+      end)
+      vim.keymap.set('n', '<C-l>', function()
+        ui.nav_file(4)
+      end)
+    end,
+  },
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
