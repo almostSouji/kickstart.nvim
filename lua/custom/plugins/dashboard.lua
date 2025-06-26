363ad216b3a65bd5f9d7f427462bf257eae0a753
almostSouji <timoqueezle@gmail.com>
2025-06-26 02:03:19 +0200

    chore(dashboard): day and nighttime banner

--- lua/custom/plugins/dashboard.lua
+++ lua/custom/plugins/dashboard.lua
@@ -6,16 +6,36 @@ return {
     local alpha = require 'alpha'
     local theta = require 'alpha.themes.theta'
     local dash = require 'alpha.themes.dashboard'
-
-    theta.header.val = {
-      [[ Z      |\      _,,,---,,_         ]],
-      [[  Z    / ,`.-'`'   -.  ;-,'-,,_    ]],
-      [[   Zz  |,4-  ) )-,_. ´\ (  `'-,##> ]],
-      [[      ' --''(_/--'  `-'\_)         ]],
+    local daytime = {
+      [[                                                       ]],
+      [[                                                       ]],
+      [[                                                       ]],
+      [[                                                       ]],
+      [[                  |\      _,,,---,,_                   ]],
+      [[                 / ,`.-'`'   -.  ;-,'-,,_              ]],
+      [[                 |,4-  ) )-,_. ´\ (  `'-,##>           ]],
+      [[                ' --''(_/--'  `-'\_)                   ]],
       [[]],
-      '        ' .. os.date '%d/%m/%Y %X',
+      '                  ' .. os.date '%d/%m/%Y %X',
     }
 
+    local nighttime = {
+      [[. ⭑  ⋆     .  ⭑        .    ✦     .     ⭑ +     ⋆  +  .]],
+      [[         ✦       ⭑ ' +    ⋆            .      .  ⭑     ]],
+      [[   ⭑.        ⭑ '  .           '   +              .⋆   ']],
+      [[   .     .             ✦        '      ⭑ '        .    ]],
+      [[       ⭑   Z      |\      _,,,---,,_          ⭑        ]],
+      [[  +         Z    / ,`.-'`'   -.  ;-,'-,,_             ⋆]],
+      [[             Zz  |,4-  ) )-,_. ´\ (  `'-,##>           ]],
+      [[                ' --''(_/--'  `-'\_)                   ]],
+      [[]],
+      '                  ' .. os.date '%d/%m/%Y %X',
+    }
+
+    local current_hour = tonumber(os.date '%H')
+    local banner = (current_hour < 8 or current_hour > 19) and nighttime or daytime
+
+    theta.header.val = banner
     -- uses syntax highlight
     -- theta.header.opts.hl = ''
 
@@ -24,10 +44,13 @@ return {
       dash.button('i', '󰠮  Notes Index', '<cmd>Neorg index<CR>'),
       dash.button('n', '󱓧  Find Notes', '<Plug>(neorg.telescope.find_norg_files)'),
       dash.button('e', '󰒲  Edit Dashboard', '<cmd>e ~/AppData/Local/nvim/lua/custom/plugins/dashboard.lua<CR>'),
-      dash.button('u', '  Update plugins', '<cmd>Lazy sync<h'),
+      dash.button('u', '  Update plugins', '<cmd>Lazy sync<CR>'),
       dash.button('q', '󰅚  Quit', '<cmd>qa<CR>'),
     }
 
     alpha.setup(theta.config)
+    vim.api.nvim_create_user_command('Dash', function(_)
+      alpha.start(false, theta.config)
+    end, { nargs = 0 })
   end,
 }
