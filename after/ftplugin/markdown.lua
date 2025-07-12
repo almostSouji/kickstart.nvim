fe409c37b24b8443dcab2c77b79dce96a1c3b173
almostSouji <timoqueezle@gmail.com>
2025-07-12 14:23:08 +0200

    feat: treesitter-context, render-markdown, dropbar

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,6 @@
+-- NOTE: just applies to markdown files
+
+local o = vim.o
+
+o.spelllang = 'en_us'
+o.spell = true
---
+++ lua/kickstart/plugins/treesitter-context.lua
@@ -0,0 +1,8 @@
+-- BUG: figure out why this doesn't work
+
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+  opts = {},
+}
+
+-- vim: ts=2 sts=2 sw=2 et
